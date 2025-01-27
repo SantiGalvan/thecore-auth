9e67be48f825d0a6a60af547debfeea38a6dece8
Santiago <s.galvancolorado@bancolini.com>
2025-01-27 08:28:41 +0000

    created Loading component

---
+++ src/components/Loading.jsx
@@ -0,0 +1,9 @@
+const Loading = () => {
+    return (
+        <div className="fixed top-0 bottom-0 left-0 right-0 flex items-center justify-center bg-[#00000080]">
+            <p className="text-white text-2xl select-none">Loading...</p>
+        </div>
+    )
+}
+
+export default Loading;
