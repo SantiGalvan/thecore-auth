1f9634c6f6be3d77d67e7f24c2cfbb0b71005697
Santiago <s.galvancolorado@bancolini.com>
2025-02-03 10:25:36 +0000

    added props

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,7 +5,39 @@ import { useAlert } from "../contexts/AlertContext";
 import LoginForm from "../components/LoginForm";
 import { useConfig } from "../contexts/ConfigContext";
 
-const Login = () => {
+const Login = (props) => {
+
+  const {
+    formStyle =  {
+      size: 'w-[800px]',
+      bgColor: 'bg-slate-50',
+      rounded: 'rounded-lg',
+      shadow: 'shadow-lg'
+    },
+    userData,
+    userInput = {
+      label: 'Email',
+      type: 'email',
+      placeholder: 'email@email.it'
+    }, 
+    title = {
+      text: 'Login',
+      size: 'text-4xl',
+      position: 'text-center',
+      spacing: 'my-12'
+    },
+    buttonStyle = {
+      position: 'justify-center',
+      spacing: 'my-12',
+      color: 'bg-blue-500',
+      hoverColor: 'hover:bg-blue-700',
+      textColor: 'text-white',
+      size: 'py-2 px-4',
+      rounded: 'rounded-full',
+      text: 'Login'
+    }
+
+  } = props;
 
   const { login } = useAuth();
   const { setShowAlert, setTypeAlert, setMessageAlert } = useAlert();
@@ -51,9 +83,15 @@ const Login = () => {
       <div className="container mx-auto flex items-center justify-center h-screen">
 
         <LoginForm 
-          submitForm={handleLogin}
-          formData={formData}
-          changeValue={changeData}
+         submitForm={handleLogin}
+         formData={formData}
+         changeValue={changeData}
+         
+         formStyle={formStyle}
+         userData={userData}
+         userInput={userInput}
+         title={title}
+         buttonStyle={buttonStyle}
         />
 
       </div>
