b17eef956d2dec1c455c6f03d887b72749a4d6df
Santiago <s.galvancolorado@bancolini.com>
2025-02-07 08:59:36 +0000

    created PackageRoutes

---
+++ src/routes/PackageRoutes.jsx
@@ -0,0 +1,47 @@
+import { Route, Routes } from "react-router-dom";
+import DefaultLayout from "../layouts/DefaultLayout";
+import Login from "../pages/Login";
+import AuthPage from "../middlewares/AuthPage";
+import Dashboard from "../pages/user/Dashboard";
+import { useRoutesInjection } from "../contexts/RouteContext";
+
+const PackageRoutes = () => {
+
+    const { publicRoutes, privateRoutes } = useRoutesInjection();
+
+    return (
+        <Routes>
+
+            <Route element={<DefaultLayout />}>
+
+                {/* Rotte pubbliche */}
+                <Route path="/">
+
+                    <Route index element={<Login />} />
+
+                    {/* Nuove rotte da inserire con il Context */}
+                    {publicRoutes.map((route, i) => (
+                        <Route key={`public-${i}`} path={route.path} element={route.element} />
+                    ))}
+
+                </Route>
+
+                {/* Rotte private */}
+                <Route element={<AuthPage />}>
+
+                    <Route path="/dashboard/:id" element={<Dashboard/>} />
+
+                    {/* Nuove rotte da inserire con il Context */}
+                    {privateRoutes.map((route, i) => (
+                        <Route key={`private-${i}`} path={route.path} element={route.element} />
+                    ))}
+
+                </Route>
+
+            </Route>
+
+        </Routes>
+    )
+}
+
+export default PackageRoutes;
