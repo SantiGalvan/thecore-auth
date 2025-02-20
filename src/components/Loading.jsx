2b762ca22c16699c0b84aadfee290045251daa71
Santiago <s.galvancolorado@bancolini.com>
2025-02-20 13:55:13 +0000

    fixed loading component

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,11 +1,11 @@
 import { ImSpinner9 } from "react-icons/im";
 
-const Loading = () => {
+const Loading = ({spinner = true, text, children, containerStyle}) => {
 
     return (
-        <div className="fixed top-0 bottom-0 left-0 right-0 flex items-center justify-center bg-gray-600 z-10">
-            <ImSpinner9 className="text-[20rem] relative animate-spin text-white" />
-            <p className="text-white text-2xl select-none absolute">Loading...</p>
+        <div className={`fixed top-0 bottom-0 left-0 right-0 flex items-center justify-center bg-loading-bg z-[999] ${containerStyle}`}>
+            {spinner && <ImSpinner9 className="text-[20rem] relative animate-spin text-spinner" />}
+            {children ? children : <p className="text-white text-2xl select-none absolute">{text || 'Loading...'}</p>}
         </div>
     )
 }
