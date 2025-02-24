92a40c26fa1e39b8df750c089afa70f3b076e763
Santiago <s.galvancolorado@bancolini.com>
2025-02-24 13:42:56 +0000

    fixed alert component

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -101,7 +101,7 @@ const Alert = () => {
 
     return(
         
-        <div className={`flex items-center p-4 pt-6 ${textColor} rounded-lg ${bgColor} mx-auto max-sm:max-w-[280px] max-sm:min-w-[200px] sm:max-w-1/3 fixed top-[calc(100vh-100px)] sm:right-10 right-1/2 translate-x-1/2 sm:translate-0 z-100`} role="alert">
+        <div className={`flex items-center p-4 pt-6 ${textColor} rounded-lg ${bgColor} mx-auto max-sm:min-w-[280px] sm:max-w-1/3 fixed top-[calc(100vh-100px)] sm:right-10 right-1/2 translate-x-1/2 sm:translate-0 z-100`} role="alert">
 
             <div className="w-full bg-gray-200 rounded-t-lg overflow-hidden h-2.5 absolute top-0 left-0 right-0">
                 <div className={`${progressColor} h-2.5 rounded-t-lg`} style={{width: `${progress}%`}}></div>
