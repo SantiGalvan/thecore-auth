0ee9364648581d45ec018f2c25108f77b41d415c
Santiago <s.galvancolorado@bancolini.com>
2025-03-21 10:16:38 +0000

    fix Login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,7 +5,7 @@ import { useLoginForm } from "../contexts/LoginFormContext";
 import { useConfig } from "../contexts/ConfigContext";
 
 
-const Login = ({Logo}) => {
+const Login = ({ Logo }) => {
 
   const { styleCardForm, styleContainerLogo, styleLogo, overrideStyle } = useLoginForm();
   const { firstPrivatePath } = useConfig();
@@ -15,10 +15,10 @@ const Login = ({Logo}) => {
   // UseEffect per controllare che l'utente loggato non entri nella pagina di login
   useEffect(() => {
 
-    const id = localStorage.getItem('id');
+    const user = JSON.parse(localStorage.getItem('user'));
     const token = localStorage.getItem('accessToken');
 
-    if(token && id)  navigate(`${firstPrivatePath}${id}`);
+    if (token && user.id) navigate(`${firstPrivatePath}${user.id}`);
 
   }, []);
 
@@ -26,7 +26,7 @@ const Login = ({Logo}) => {
     <section id="login-page">
 
       <div className={overrideStyle.container || `container mx-auto flex items-center justify-center h-screen`}>
-        
+
         <div className={overrideStyle.cardForm || `bg-form card-style card-size flex flex-col sm:flex-row sm:items-center justify-center ${styleCardForm}`}>
 
           <div className={overrideStyle.containerLogo || `basis-1/2 flex items-center justify-center ${styleContainerLogo}`}>
