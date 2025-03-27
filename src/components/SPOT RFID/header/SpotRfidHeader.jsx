42561e9f8c251371da9296c3e861b01ab30a6107
SantiGalvan <santiagogalvancolorado@gmail.com>
2025-03-27 10:58:19 +0000

    add new variables

--- src/components/SPOT RFID/header/SpotRfidHeader.jsx
+++ src/components/SPOT RFID/header/SpotRfidHeader.jsx
@@ -45,7 +45,7 @@ const Header = ({ Logo }) => {
 
                     {/* Bottone Logout */}
                     {(!autoLogin && matchHome) && <button
-                        className="h-[var(--header-logo-size)] w-[var(--header-logo-size)] flex items-center justify-center bg-red-600 text-white rounded-lg shadow-md transition duration-200 ease-in-out transform active:translate-y-[2px] cursor-pointer"
+                        className="h-[48px] w-[48px] flex items-center justify-center bg-red-600 text-white rounded-lg shadow-md transition duration-200 ease-in-out transform active:translate-y-[2px] cursor-pointer"
                         type="button"
                         onClick={signOut}
                     >
@@ -54,7 +54,7 @@ const Header = ({ Logo }) => {
 
                     {/* Bottone per tornare indietro */}
                     {(!matchHome && showHeaderButton) && <button
-                        className="h-[var(--header-logo-size)] w-[var(--header-logo-size)] flex items-center justify-center bg-sky-600 text-white rounded-lg shadow-md transition duration-200 ease-in-out transform active:translate-y-[2px] cursor-pointer"
+                        className="h-[48px] w-[48px] flex items-center justify-center bg-sky-600 text-white rounded-lg shadow-md transition duration-200 ease-in-out transform active:translate-y-[2px] cursor-pointer"
                         type="button"
                         onClick={() => navigate(-1)}
                     >
--- src/components/SPOT RFID/inputs/InputGroup.jsx
+++ src/components/SPOT RFID/inputs/InputGroup.jsx
@@ -12,7 +12,7 @@ const InputGroup = ({ isTag = false, value, label, placeholder, id, onChange, er
                 inputValue={value || ''}
                 disabled={isTag}
                 inputChange={onChange}
-                overrideStyle={`${isTag ? 'w-full' : 'basis-2/3'} ${error ? 'border-red-600 focus:ring focus:ring-red-600 focus:border-red-600 focus:shadow-[0_0_0_4px_rgba(255,0,0,0.5)]' : ''} bg-input-bg border border-input-border text-input-text text-input-placeholder input-rounded focus:ring focus:ring-[#16693880] focus:border-[#166938] focus:outline-none focus:shadow-[0_0_0_4px_#16693880] block w-full p-input`}
+                overrideStyle={`${isTag ? 'w-full' : 'basis-2/3'} ${error ? 'border-red-600 focus:ring focus:ring-red-600 focus:border-red-600 focus:shadow-[0_0_0_4px_rgba(255,0,0,0.5)]' : ''} bg-input-bg border border-input-border text-input-text text-input-placeholder input-rounded focus:ring focus:ring-focus-ring focus:border-spot-rfid-input-border focus:outline-none focus:shadow-spot-rfid-input block w-full p-input`}
             />
             {error && <p className="text-red-600 text-[13px] absolute bottom-0 left-26 translate-y-[20px]">{error}</p>}
         </div>
