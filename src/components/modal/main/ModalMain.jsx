66bbfb546be338fe7720b5aed8fcfc81c84db708
SantiGalvan <santiagogalvancolorado@gmail.com>
2025-09-02 09:42:47 +0000

    fix type

--- src/components/modal/main/ModalMain.jsx
+++ src/components/modal/main/ModalMain.jsx
@@ -5,7 +5,7 @@ const ModalMain = ({ type, children, item, overrideStyle }) => {
                 type !== 'delete' &&
                 <main className={overrideStyle || 'my-8 max-h-[600px] overflow-y-auto overflow-x-hidden'}>
 
-                    {(type === 'submit' || type === 'delete') && !item ?
+                    {type === 'delete' && !item ?
                         (
                             <div className="text-red-600 font-semibold">Errore: nessun item selezionato per l'operazione.</div>
                         )
