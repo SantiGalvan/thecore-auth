5e23cbc37f53615246f29966cd72a4d94ed1a150
SantiGalvan <santiagogalvancolorado@gmail.com>
2025-12-04 13:18:12 +0000

    create hook title page

---
+++ src/hooks/title/UsePageTitle.jsx
@@ -0,0 +1,23 @@
+import { useLayoutEffect } from "react";
+import { matchPath, useLocation } from "react-router-dom";
+
+const UsePageTitle = (routes = [], defaultTitle = "MyApp") => {
+    const location = useLocation();
+
+    useLayoutEffect(() => {
+        let matchedTitle = defaultTitle;
+
+        for (const route of routes) {
+            // matchPath gestisce anche route dinamiche come /dashboard/:id
+            if (matchPath({ path: route.path, end: true }, location.pathname)) {
+                matchedTitle = route.title;
+                break;
+            }
+        }
+
+        document.title = matchedTitle;
+    }, [location, routes, defaultTitle]);
+
+}
+
+export default UsePageTitle;
