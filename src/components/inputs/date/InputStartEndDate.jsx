03a6614557032f509a22312f44698ad0a17a0122
SantiGalvan <santiagogalvancolorado@gmail.com>
2026-01-29 08:26:24 +0000

    import input date component

--- src/components/inputs/date/InputStartEndDate.jsx
+++ src/components/inputs/date/InputStartEndDate.jsx
@@ -1,3 +1,5 @@
+import InputDate from "./InputDate";
+
 const InputStartEndDate = (props) => {
 
     const {
@@ -27,46 +29,35 @@ const InputStartEndDate = (props) => {
         <div className={containerStyle || `flex gap-4 mx-auto w-full mb-4`}>
 
             {/* Start Date */}
-            <div className={startContainerStyle || `flex flex-col gap-1 mx-auto w-full`}>
-                <label
-                    htmlFor={startId}
-                    className={labelStyle || `mb-1 text-input-label font-medium text-color-label`}
-                >
-                    {startTitle}
-                </label>
-                <input
-                    type="date"
-                    id={startId}
-                    name={startName}
-                    value={startValue || ""}
-                    onChange={onChange}
-                    disabled={disabled}
-                    required={required}
-                    className={startStyle || `bg-input-bg border border-input-border rounded-lg text-input-text placeholder:text-input-placeholder rounded-input focus:ring focus:ring-primary focus:border-primary focus:outline-none focus:shadow-[var(--shadow-primary-input)] block w-full h-[43px] p-input disabled:bg-gray-100 disabled:text-gray-400 disabled:cursor-not-allowed disabled:opacity-60`}
-                />
-            </div>
+            <InputDate
+                id={startId}
+                name={startName}
+                value={startValue}
+                onChange={onChange}
+                disabled={disabled}
+                required={required}
+                title={startTitle}
+                labelStyle={labelStyle}
+                containerStyle={startContainerStyle}
+                inputStyle={startStyle}
+            />
 
             {/* End Date */}
             {endDateShow &&
-                <div className={endContainerStyle || `flex flex-col gap-1 mx-auto w-full`}>
-                    <label
-                        htmlFor={endId}
-                        className={labelStyle || `mb-1 text-input-label font-medium text-color-label`}
-                    >
-                        {endTitle}
-                    </label>
-                    <input
-                        type="date"
-                        id={endId}
-                        name={endName}
-                        value={endValue || ""}
-                        onChange={onChange}
-                        disabled={disabled}
-                        required={required}
-                        className={endStyle || `bg-input-bg border rounded-lg text-input-text placeholder:text-input-placeholder  rounded-input focus:ring focus:ring-primary focus:border-primary focus:outline-none  focus:shadow-[var(--shadow-primary-input)] block w-full h-[43px] p-input  disabled:bg-gray-100 disabled:text-gray-400 disabled:cursor-not-allowed disabled:opacity-60 ${dateError ? "border-red-500" : "border-input-border"}`}
-                    />
+                <InputDate
+                    id={endId}
+                    name={endName}
+                    value={endValue}
+                    onChange={onChange}
+                    disabled={disabled}
+                    required={required}
+                    title={endTitle}
+                    labelStyle={labelStyle}
+                    containerStyle={endContainerStyle}
+                    inputStyle={endStyle}
+                >
                     {dateError && <p className="text-red-500 text-[13px] mt-1">{dateError}</p>}
-                </div>
+                </InputDate>
             }
 
             {children}
