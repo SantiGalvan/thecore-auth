c6974e2cfb0c5f61c0e4391c2c62faf5515f3132
SantiGalvan <santiagogalvancolorado@gmail.com>
2026-02-05 08:14:49 +0000

    remove id

--- src/hooks/auth/useAuthStorage.jsx
+++ src/hooks/auth/useAuthStorage.jsx
@@ -3,15 +3,13 @@ import { useStorage } from "../storage/useStorage";
 const useAuthStorage = () => {
     const [token, setToken, removeToken] = useStorage(null, 'accessToken');
     const [user, setUser, removeUser] = useStorage(null, 'user');
-    const [id, setId, removeId] = useStorage(null, 'id');
 
     const storageLogout = () => {
         removeToken();
         removeUser();
-        removeId();
     }
 
-    return { token, user, id, setToken, setUser, setId, storageLogout };
+    return { token, user, setToken, setUser, storageLogout };
 }
 
 export { useAuthStorage };
