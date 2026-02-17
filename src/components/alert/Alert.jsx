2d429e1798ac36939a9ad6486d3a14bf718778cf
SantiGalvan <santiagogalvancolorado@gmail.com>
2026-02-17 16:32:25 +0000

    remove border

--- src/components/alert/Alert.jsx
+++ src/components/alert/Alert.jsx
@@ -46,7 +46,7 @@ const Alert = () => {
     return (
 
         // Contenitore dell'Alert
-        <div className={` ${bgColor} ${textColor} flex items-center min-h-[65px] max-h-[100px] p-4 pt-6 border-2 rounded-lg mx-auto max-sm:min-w-[240px] sm:max-w-1/3 fixed sm:top-[calc(100vh-100px)] top-[calc(100vh-115px)] sm:right-10 right-1/2 translate-x-1/2 sm:translate-0 z-100`} role="alert">
+        <div className={` ${bgColor} ${textColor} flex items-center min-h-[65px] max-h-[100px] p-4 pt-6 rounded-lg mx-auto max-sm:min-w-[240px] sm:max-w-1/3 fixed sm:top-[calc(100vh-100px)] top-[calc(100vh-115px)] sm:right-10 right-1/2 translate-x-1/2 sm:translate-0 z-100`} role="alert">
 
             {/* Progress bar */}
             <div className="w-full bg-gray-200 rounded-t-lg overflow-hidden h-2.5 absolute top-0 left-0 right-0">
