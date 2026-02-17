e8bd967c6bb301aa6aab3427cd110e67f4d29da1
SantiGalvan <santiagogalvancolorado@gmail.com>
2026-02-17 14:50:57 +0000

    create new hook

---
+++ src/hooks/orientation/useOrientation.jsx
@@ -0,0 +1,19 @@
+import { useState, useEffect } from 'react';
+
+const useOrientation = () => {
+  const [orientation, setOrientation] = useState(
+    window.innerWidth > window.innerHeight ? 'landscape' : 'portrait'
+  );
+
+  useEffect(() => {
+    const handleResize = () => {
+      setOrientation(window.innerWidth > window.innerHeight ? 'landscape' : 'portrait');
+    };
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return orientation;
+}
+
+export default useOrientation;
