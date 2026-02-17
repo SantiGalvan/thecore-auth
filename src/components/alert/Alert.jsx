fbd30781987e82b163908e77e733fd6fada8d81e
SantiGalvan <santiagogalvancolorado@gmail.com>
2026-02-17 16:16:37 +0000

    fix alert component

--- src/components/alert/Alert.jsx
+++ src/components/alert/Alert.jsx
@@ -45,18 +45,23 @@ const Alert = () => {
 
     return (
 
-        <div className={`flex items-center p-4 pt-6 ${textColor} rounded-lg ${bgColor} mx-auto max-sm:min-w-[280px] sm:max-w-1/3 fixed top-[calc(100vh-100px)] sm:right-10 right-1/2 translate-x-1/2 sm:translate-0 z-100`} role="alert">
+        // Contenitore dell'Alert
+        <div className={` ${bgColor} ${textColor} flex items-center min-h-[65px] max-h-[100px] p-4 pt-6 border-2 rounded-lg mx-auto max-sm:min-w-[240px] sm:max-w-1/3 fixed sm:top-[calc(100vh-100px)] top-[calc(100vh-115px)] sm:right-10 right-1/2 translate-x-1/2 sm:translate-0 z-100`} role="alert">
 
+            {/* Progress bar */}
             <div className="w-full bg-gray-200 rounded-t-lg overflow-hidden h-2.5 absolute top-0 left-0 right-0">
                 <div className={`${progressColor} h-2.5 rounded-t-lg`} style={{ width: `${progress}%` }}></div>
             </div>
 
+            {/* Icona dell'alert */}
             {getIcon(typeAlert)}
 
+            {/* Messaggio dell'alert */}
             <div className="px-4 text-sm font-medium">
                 {messageAlert}
             </div>
 
+            {/* Bottone per chiudere l'alert */}
             <button type="button" className={`ms-auto ${buttonBg} rounded-lg focus:ring-2 ${focusRing} p-1.5 ${hoverBg} inline-flex items-center justify-center h-8 w-8 cursor-pointer`}
                 title="Close"
                 onClick={closeAlert}
